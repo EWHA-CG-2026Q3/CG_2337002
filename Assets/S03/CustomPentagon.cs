1c7650e765c3dc8f9e68aac4051f168a638335d7
minnngo <mingo0273@gmail.com>
2026-09-10 14:06:48 +0900

    S03: 오각형 메시 방향 수정

--- Assets/S03/CustomPentagon.cs
+++ Assets/S03/CustomPentagon.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class CustomPentagon : MonoBehaviour
 {
-    void Start()
-    {
-        Vector3[] vertices = new Vector3[]
-       {
+  void Start()
+  {
+    Vector3[] vertices = new Vector3[]
+   {
            new Vector3(-1f, -1f, 0f), // 0
             new Vector3(-1f,  1f, 0f), // 1
             new Vector3( 0f,  2f, 0f), // 2
             new Vector3( 1f,  1f, 0f), // 3
             new Vector3( 1f, -1f, 0f), // 4
-       };
+   };
 
-        int[] triangles = new int[]
-          {
+    int[] triangles = new int[]
+      {
             0, 1, 2, // Triangle 1
             0, 2, 3, // Triangle 2
             0, 3, 4  // Triangle 3
-          };
+      };
 
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
+    Mesh mesh = new Mesh();
+    mesh.vertices = vertices;
+    mesh.triangles = triangles;
+    mesh.RecalculateNormals();
 
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-    }
+    GetComponent<MeshFilter>().mesh = mesh;
+    GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+  }
 
 }
