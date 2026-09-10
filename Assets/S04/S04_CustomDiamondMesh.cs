bbd0d0894b33c1d2f34006a812c538347e69b4dc
minnngo <mingo0273@gmail.com>
2026-09-10 12:05:50 +0900

    S04: 다이아몬드 상단 첫번째 삼각형 추가

--- Assets/S04/S04_CustomDiamondMesh.cs
+++ Assets/S04/S04_CustomDiamondMesh.cs
@@ -21,6 +21,7 @@ public class S04_CustomDiamondMesh : MonoBehaviour
         int[] triangles = new int[]
         {
             // 위쪽 4면 (정점 4 사용)
+            4, 1, 0,
 
             // 아래쪽 4면 (정점 5 사용)
         };
